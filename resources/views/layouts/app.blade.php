ad55fbcab3071402668f3ed2fcf7d67c61bf4e86
eardakbekov1 <eardakbekov@gmail.com>
2024-12-16 15:28:16 +0600

    PHP version changed to 8.2, Bootstrap cdn is connected

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <script>
         toastr.options = {
